89c6a1b69a98063c4d074628b521ee8c3632334e
Dlawlet <Dlawlet@protonmail.com>
2023-05-26 20:29:01 +0200

    fake improve

--- main.jl
+++ main.jl
@@ -1,43 +1,71 @@
+using JuMP
+using Gurobi
+
 function positive_weight_cycle_detection(G, ω)
-    V = vertices(G)
-    E = edges(G)
-    n = length(V)
-    d = zeros(n)
-    father = zeros(Int, n)
-    C = []
-    
-    for i in 1:n
-        d[i] = Inf
-    end
-    
-    for (u, v) in V × V
-        d[u] = 0
-        for (u, v) in E
-            d[v] = ω[(u, v)]
-            father[v] = u
+    V = 1:size(G)[1]
+    E = [(u, v) for u in V, v in V if G[u, v] == 1] # set of edges
+
+    model = Model(optimizer_with_attributes(Gurobi.Optimizer, "OutputFlag" => 0))
+
+    @variable(model, d[u in V] >= 0)   # Distance variables
+    @variable(model, f[u in V, v in V] >=0 )   # Indicator variables for edges in the cycle
+
+    # Objective: Minimize the total distance
+    @objective(model, Min, sum(d[u] for u in V))
+
+    # Constraints: Distance update
+    for u in V
+        for v in V
+            @constraint(model, d[u] <= d[v] + ω[u, v])
         end
     end
-    
-    for w in V
-        for (u, v) in V × V
-            if d[u] > d[w] + ω[(w, v)]
-                d[u] = d[w] + ω[(w, v)]
-                father[u] = father[v]
-                
-                if u == v && d[u] < 0
-                    while true
-                        w = father[u]
-                        push!(C, (w, v))
-                        v = w
-                        if v == u
-                            break
-                        end
-                    end
-                    return C
-                end
+
+    # Constraints: Flow conservation
+    for u in V
+        @constraint(model, sum(f[u, v] for v in V) - sum(f[v, u] for v in V) == 0)
+    end
+
+    # Constraint: Positive weight cycle
+    @constraint(model, sum(ω[u, v] * f[u, v] for (u, v) in E) >= 1e-6)
+
+    optimize!(model)
+
+    if termination_status(model) == MOI.OPTIMAL
+        cycle = []
+        for (u, v) in E
+            println("f[$u, $v] = ", value(f[u, v]))
+            if value(f[u, v]) > 0
+                push!(cycle, (u, v))
             end
         end
+        return cycle
+    else
+        return []
     end
-    
-    return "There is no positive weight cycle in G"
+end
+
+# Test
+
+G = ([0 1 0 0 0 0;
+     1 0 0 0 0 0;
+     0 0 0 1 0 0;
+     0 0 1 0 1 0;
+     0 0 1 0 0 0;
+     0 0 0 0 1 0])
+
+ω = ([0.5 0.6 0 0 0 0;
+     0.8 0.4 0 0 0 0;
+     0 0 0 0.4 0 0;
+     0 0 0.5 0 0.5 0;
+     0 0 0.7 0 0 0;
+     0 0 0 0 0.9 0.8])
+# Call the positive_weight_cycle_detection function
+cycle = positive_weight_cycle_detection(G, ω)
+
+# Check if a positive weight cycle was found
+if isempty(cycle)
+    println("No positive weight cycle found.")
+else
+    println("Positive weight cycle found:")
+    println(cycle)
 end
--- test.jl
+++ test.jl
@@ -1,1 +1,72 @@
-print(1+1)
+function positive_weight_cycle_detection(G, W)
+    Cycle = []
+    V = 1:size(G, 1) # set of vertices
+    E = [(u, v) for u in V, v in V if G[u, v] == 1] # set of edges
+
+    # distance matrix of size |V| x |V|
+    d = fill(Inf, size(G))
+
+    father = fill(0, size(G))
+
+    for v in V
+        d[v, v] = 0
+    end
+    for (u, v) in E
+        d[u, v] = W[u, v]
+        d[v, u] = -W[u, v]  # Reverse direction with negated weight
+        father[u, v] = u
+        father[v, u] = v  # Reverse direction
+    end
+
+    for w in V
+        for u in V
+            for v in V
+                if d[u, v] > d[u, w] + d[w, v]
+                    d[u, v] = d[u, w] + d[w, v]
+                    father[u, v] = father[w, v]
+                end
+                if u == v && d[u, u] < 0
+                    w = father[u, v]
+                    push!(Cycle, (w, v))
+                    v = w
+                    while v != u
+                        w = father[u, v]
+                        push!(Cycle, (w, v))
+                        v = w
+                    end
+                    return Cycle    
+                end
+                
+            end
+        end
+    end
+
+    return Cycle
+end
+
+# Test
+
+G = transpose([0 1 0 0 0 0;
+     1 0 0 0 0 0;
+     0 0 0 1 0 0;
+     0 0 1 0 1 0;
+     0 0 1 0 0 0;
+     0 0 0 0 1 0])
+
+ω = transpose([0.5 0.6 0 0 0 0;
+     0.8 0.4 0 0 0 0;
+     0 0 0 0.4 0 0;
+     0 0 0.5 0 0.5 0;
+     0 0 0.7 0 0 0;
+     0 0 0 0 0.9 0.8])
+
+# Call the positive_weight_cycle_detection function
+cycle = positive_weight_cycle_detection(G, ω)
+
+# Check if a positive weight cycle was found
+if isempty(cycle)
+    println("No positive weight cycle found.")
+else
+    println("Positive weight cycle found:")
+    println(cycle)
+end
